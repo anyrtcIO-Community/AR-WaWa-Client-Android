243bc324c723128947ae8d1eed19261149d43523
liuxiaozhong <liuxiaozhong@dync.cc>
2019-05-15 14:22:05 +0800

    commit 3.0

--- app/src/main/java/org/ar/wawa/WaWaApplication.java
+++ app/src/main/java/org/ar/wawa/WaWaApplication.java
@@ -2,6 +2,7 @@ package org.ar.wawa;
 
 import android.app.Application;
 
+import org.ar.DeveloperInfo;
 import org.ar.rtcp_kit.ARRtcpEngine;
 import org.ar.tools.SoundPlayUtils;
 import org.ar.wawaClient.ARWaWaClient;
@@ -15,8 +16,8 @@ public class WaWaApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        ARRtcpEngine.Inst().initEngineWithARInfo(getApplicationContext(),"95878050", "anyrtcy0UkK1PZydLX", "q50MGCmTkDn/wXBWNY5YjnB54KOzwwV8RoQ02AV6R9Y", "fabf75cfe0090ef495bf628cd7b9270b");
-        ARWaWaClient.getInstance().initEngineWithARInfo("95878050","anyrtcy0UkK1PZydLX","q50MGCmTkDn/wXBWNY5YjnB54KOzwwV8RoQ02AV6R9Y","fabf75cfe0090ef495bf628cd7b9270b");
+        ARRtcpEngine.Inst().initEngineWithARInfo(getApplicationContext(), DeveloperInfo.DEVELOPERID, DeveloperInfo.APPID, DeveloperInfo.APPKEY, DeveloperInfo.APPTOKEN);
+        ARWaWaClient.getInstance().initEngineWithARInfo(DeveloperInfo.DEVELOPERID, DeveloperInfo.APPID, DeveloperInfo.APPKEY, DeveloperInfo.APPTOKEN);
         SoundPlayUtils.init(this);
     }
 
